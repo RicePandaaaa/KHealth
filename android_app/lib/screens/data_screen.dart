3b411823e7285fb9a315a2c8987aeaa713baf62c
RicePandaaaa <anthony.ha.pham@tamu.edu>
2025-01-24 16:57:32 -0600

    Start replacing filler image with actual parts on data screen

--- android_app/lib/screens/data_screen.dart
+++ android_app/lib/screens/data_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
+String imageLink = "assets/images/daily_readings.png";
+
 class DataScreen extends StatelessWidget {
   const DataScreen({super.key});
 
@@ -38,7 +40,262 @@ class DataScreen extends StatelessWidget {
             ),
 
             Image.asset(
-              "assets/images/data_screen_filler.png"
+              imageLink
+            ),
+
+            const Divider(
+              color: Colors.white,
+              thickness: 2,
+              height: 30
+            ),
+
+            const Text(
+              "Daily Levels (mg/dL)",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 7),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Min: 65",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Avg: 90",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Max: 100",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const Divider(
+              color: Colors.white,
+              thickness: 2,
+              height: 30
+            ),
+
+            const Text(
+              "Weekly Levels (mg/dL)",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+                        Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 7),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Min: 65",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Avg: 90",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Max: 100",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const Divider(
+              color: Colors.white,
+              thickness: 2,
+              height: 30
+            ),
+
+            const Text(
+              "Monthly Levels (mg/dL)",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+                        Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 7),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Min: 65",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Avg: 90",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    width: 125,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4CD8B7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Max: 100",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
 
             const Divider(
