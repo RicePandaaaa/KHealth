78574bd3cd5ad448a6f6ae6178e3e6586f75aa0a
RicePandaaaa <anthony.ha.pham@tamu.edu>
2025-04-24 14:37:00 -0500

    Update model

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-from math import pi
-
-F = 120
-D_w = 0.15
-C = 9
-
-def K(C):
-    return (4 * C - 1) / (4 * C - 4) + (0.615 / C)
-
-tau = 8 * K(C) * F * C / (pi * D_w ** 2)
-
-print(tau)
-
-while tau > 280000:
-    C += 1
-    tau = 8 * K(C) * F * C / (pi * D_w ** 2)
-    print(tau)
+mystr = "cantaloupe"
+mylist = mystr.split("a")
+for i in mylist:
+	print(i[-1], end="u")
